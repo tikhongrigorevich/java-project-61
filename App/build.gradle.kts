8499bc474f86f0c6189bd9b4442e17f2cc72ba60
Tikhon Pilipchuk <tikhon.pilipchuk@gmail.com>
2026-06-17 20:01:22 +0300

    update build.gradle.kts file

--- App/build.gradle.kts
+++ App/build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
     id("java")
+    id("application")
+    id("se.patrikerdes.use-latest-versions") version "0.2.19"
+    id("com.github.ben-manes.versions") version "0.54.0"
 }
 
 group = "hexlet-code"
