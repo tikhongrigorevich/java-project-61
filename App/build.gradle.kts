fa89fb742c1f5623574e2f2595b98c81b99ec75c
Tikhon Pilipchuk <tikhon.pilipchuk@gmail.com>
2026-07-27 19:07:52 +0300

    update: add checkstyle plugin a dependencies

--- App/build.gradle.kts
+++ App/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("application")
     id("se.patrikerdes.use-latest-versions") version "0.2.19"
     id("com.github.ben-manes.versions") version "0.54.0"
+    id("checkstyle")
 }
 
 application {
@@ -16,10 +17,16 @@ repositories {
     mavenCentral()
 }
 
+checkstyle {
+    toolVersion = "10.12.4"
+}
+
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    checkstyle("com.puppycrawl.tools:checkstyle:${checkstyle.toolVersion}")
+    checkstyle("group:artifact:version")
 }
 
 tasks.test {
