911a403d3cf9e5ec9f55bf19ced370b18d0cb37c
Tikhon Pilipchuk <tikhon.pilipchuk@gmail.com>
2026-08-07 20:48:13 +0300

    update: add a new game EvenOrOdd

--- App/src/main/java/hexlet/code/App.java
+++ App/src/main/java/hexlet/code/App.java
@@ -1,7 +1,24 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        Cli.getTalkingToUser();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("2 - Even");
+        System.out.println("0 - Exit");
+        System.out.print("Your choice: ");
+        String choiceOfUser = scanner.next();
+        
+        if (choiceOfUser.equals("1")) {
+            Cli.getTalkingToUser();
+        } else if (choiceOfUser.equals("2")) {
+            EvenOrOdd.startParityGame();
+        } else if (choiceOfUser.equals("0")) {
+            scanner.close();
+        }
     }
 }
