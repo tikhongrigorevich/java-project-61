bb5019269314fd4bc157329f44515469efd25fdd
Tikhon Pilipchuk <tikhon.pilipchuk@gmail.com>
2026-08-25 21:03:29 +0300

    update App.java

--- App/src/main/java/hexlet/code/App.java
+++ App/src/main/java/hexlet/code/App.java
@@ -2,24 +2,24 @@ package hexlet.code;
 
 import hexlet.code.games.Calculator;
 import hexlet.code.games.Even;
+import hexlet.code.games.GCD;
 
 import static hexlet.code.Engine.SCANNER;
 
 public class App {
     public static void main(String[] args) {
 
-        System.out.println("Please enter the game number and press Enter.\n"
-                + "1 - Greet\n"
-                + "2 - Even\n"
-                + "3 - Calc\n"
-                + "0 - Exit");
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet\n2 - Even\n3 - Calc\n4 - GCD\n0 - Exit");
         System.out.print("Your choice: ");
         int choiceOfUser = SCANNER.nextInt();
         SCANNER.nextLine();
+
         switch (choiceOfUser) {
             case 1 -> Cli.getTalkingToUser();
             case 2 -> Even.startOfTheGame();
             case 3 -> Calculator.startOfTheGame();
+            case 4 -> GCD.startOfTheGame();
             case 0 -> SCANNER.close();
             default -> System.out.println("Invalid choice. Please try again.");
         }
